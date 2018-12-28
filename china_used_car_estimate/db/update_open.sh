95d71702bf5829284abc5f367bf6ee50e0617071
leidengjun <m18349125880@gmail.com>
2018-12-28 22:27:15 +0800

    Mod sql script

--- china_used_car_estimate/db/update_open.sh
+++ china_used_car_estimate/db/update_open.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/mysql/bin/
-mysql -h10.45.138.200 -uvaluate_user -p'ldj_DEV_~!_0705' pingjia -e"replace into open_category select * from china_used_car_estimate.base_standard_open_category;"
-mysql -h10.45.138.200 -uvaluate_user -p'ldj_DEV_~!_0705' pingjia -e"replace into open_model_detail select * from china_used_car_estimate.base_standard_open_model_detail;"
+mysql -h10.45.138.200 -uvaluate_user -p'ldj_DEV_~!_0705' pingjia -e"truncate table open_category;insert into open_category select * from china_used_car_estimate.base_standard_open_category;"
+mysql -h10.45.138.200 -uvaluate_user -p'ldj_DEV_~!_0705' pingjia -e"truncate table open_model_detail;insert  into open_model_detail select * from china_used_car_estimate.base_standard_open_model_detail;"
